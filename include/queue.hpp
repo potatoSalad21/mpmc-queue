88bf689fa6f11cfd604226ad0d4fe82afa7119c2
potatoSalad21 <eeperpeepo@gmail.com>
2026-08-18 12:56:58 +0400

    initial commit

---
+++ include/queue.hpp
@@ -0,0 +1,8 @@
+
+template <typename T>
+class MpmcBoundedQueue {
+public:
+
+private:
+
+};
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "hei" << std::endl;
+    return 0;
+}
